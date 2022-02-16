b671057f23bc28d329fc466e4add32d3ff25bf01
Simon <simonbesteiro@gmail.com>
2022-02-16 15:30:46 +0100

    red

--- src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
+++ src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
@@ -5,6 +5,7 @@ open Xunit
 [<Theory>]
 [<InlineData("1", 1)>]
 [<InlineData("2", 2)>]
+[<InlineData("Fizz", 3)>]
 let ``convert input to string`` (expected: string, input: int) =
     let result = FizzBuzz.convert input
     Assert.Equal(expected, result)
