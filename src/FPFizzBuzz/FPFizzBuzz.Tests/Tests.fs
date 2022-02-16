073785e67664c7134436ddbaec17f70e010722ee
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-16 15:35:18 +0100

    refactor + red

--- src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
+++ src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
@@ -5,7 +5,21 @@ open Xunit
 [<Theory>]
 [<InlineData("1", 1)>]
 [<InlineData("2", 2)>]
-[<InlineData("Fizz", 3)>]
 let ``convert input to string`` (expected: string, input: int) =
     let result = FizzBuzz.convert input
     Assert.Equal(expected, result)
+    
+    
+[<Theory>]
+[<InlineData("Fizz", 3)>]
+let ``convert input to fizz`` (expected: string, input: int) =
+    let result = FizzBuzz.convert input
+    Assert.Equal(expected, result)
+    
+    
+[<Theory>]
+[<InlineData("Buzz", 5)>]
+let ``convert input to buzz`` (expected: string, input: int) =
+    let result = FizzBuzz.convert input
+    Assert.Equal(expected, result)
+    
--- src/FPFizzBuzz/FPFizzBuzz/Library.fs
+++ src/FPFizzBuzz/FPFizzBuzz/Library.fs
@@ -1,4 +1,4 @@
 ﻿module FizzBuzz
 
 let convert number =
-    if number = 2 then "2" else if 3 = number then "Fizz" else "1"
+    if 3 = number then "Fizz" else $"{number}"
