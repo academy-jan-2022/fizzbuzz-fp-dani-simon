b893e2b0e6f10d932f3be6608b006d6a51fa5643
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-16 16:07:03 +0100

    green

--- src/FPFizzBuzz/FPFizzBuzz/Library.fs
+++ src/FPFizzBuzz/FPFizzBuzz/Library.fs
@@ -1,8 +1,17 @@
 ﻿module FizzBuzz
 
+open Microsoft.FSharp.Core
+
+
+let (|IsMultipleOfFizz|_|) input =
+    match (input % 3) with
+    | 0 -> Some "Fizz"
+    | _ -> None
+
 let convert =
     function 
     | input when input % 15 = 0 -> "FizzBuzz"
     | input when input % 5 = 0 -> "Buzz"
-    | input when input % 3 = 0 -> "Fizz"
+    | IsMultipleOfFizz s -> s
     | input -> $"{input}"
+
