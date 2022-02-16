4ebc21cb6e0f21a9d32af0fe1c1ae7b8cd508645
Simon <simonbesteiro@gmail.com>
2022-02-16 15:52:27 +0100

    red

--- src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
+++ src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
@@ -20,12 +20,15 @@ let ``convert input to fizz`` (expected: string, input: int) =
     
 [<Theory>]
 [<InlineData("Buzz", 5)>]
+[<InlineData("Buzz", 10)>]
 let ``convert input to buzz`` (expected: string, input: int) =
     let result = FizzBuzz.convert input
     Assert.Equal(expected, result)
     
 [<Theory>]
 [<InlineData("FizzBuzz", 15)>]
+[<InlineData("FizzBuzz", 30)>]
+
 let ``convert input to fizzbuzz`` (expected: string, input: int) =
     let result = FizzBuzz.convert input
     Assert.Equal(expected, result)
