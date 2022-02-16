af687e9290bf8f2ee8cda06445fae80001e495a4
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-16 16:44:02 +0100

    green

--- src/FPFizzBuzz/FPFizzBuzz/Library.fs
+++ src/FPFizzBuzz/FPFizzBuzz/Library.fs
@@ -3,15 +3,15 @@
 open Microsoft.FSharp.Core
 
 
-let (|IsMultipleOfFizz|_|) input =
-    match (input % 3) with
-    | 0 -> Some "Fizz"
+let (|IsMultipleOf|_|) multiple input =
+    match (input % multiple) with
+    | 0 -> Some()
     | _ -> None
 
 let convert =
     function 
-    | input when input % 15 = 0 -> "FizzBuzz"
-    | input when input % 5 = 0 -> "Buzz"
-    | IsMultipleOfFizz s -> s
+    | IsMultipleOf 15 -> "FizzBuzz"
+    | IsMultipleOf 5 -> "Buzz"
+    | IsMultipleOf 3 -> "Fizz"
     | input -> $"{input}"
 
