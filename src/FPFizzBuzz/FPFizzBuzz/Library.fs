664a99b55fa288209f6a5f0622cdde521891853d
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-16 15:41:56 +0100

    green

--- src/FPFizzBuzz/FPFizzBuzz/Library.fs
+++ src/FPFizzBuzz/FPFizzBuzz/Library.fs
@@ -2,5 +2,6 @@
 
 let convert number =
     if 3 = number then "Fizz" else
-        if 5 = number then "Buzz"
+        if 5 = number then "Buzz" else
+        if 15 = number then "FizzBuzz"
         else $"{number}"
